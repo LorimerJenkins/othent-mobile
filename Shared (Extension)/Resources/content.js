77caaf5d43cd7ae521fe9dd1086d9bd2f6ced39e
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-26 14:34:23 +0100

    feat: button swift handling

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,26 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
 
+
+
+// detect reqs
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    
+    console.log("Request just in: ", request);
+    
+    if (request.command === "logInButton") {
+        console.log("logInButton")
+    }
+    
+    
+    if (request.update === "siteURL") {
+        let titleContent = window.location.href;
+        titleContent = titleContent.replace(/^https:\/\//i, "");
+        console.log(titleContent)
+        sendResponse({
+            title: titleContent
+        })
+    }
+    
+    
+    
+    
 });
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,6 +1,28 @@
-console.log("Hello World!", browser);
-console.log(1234)
-
-function Hellow() {
-    console.log(1234)
+// active tab
+function getActiveTab() {
+    return browser.tabs.query({active: true, currentWindow: true});
 }
+
+
+// log in button
+document.getElementById("logInButton").addEventListener("click", function() {
+    getActiveTab().then((tabs) => {
+        browser.tabs.sendMessage(tabs[0].id, {
+            command: "logInButton",
+        });
+    });
+});
+
+
+// connected to, text
+getActiveTab().then(async (tabs) => { // add async keyword here
+
+    let response = await browser.tabs.sendMessage(tabs[0].id, { // replace "tabs" with "browser.tabs"
+        update: 'siteURL'
+    });
+    console.log(response);
+    let titleElement = document.getElementById("title")
+    titleElement.textContent = response?.title;
+
+});
+
